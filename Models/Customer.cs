a4ef83d65ab301d6cd477f94f64709434850154a
Rebecca Robison <rebeccahrobison@outlook.com>
2023-11-27 11:46:22 -0600

    Added endpoints for all classes

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,8 +1,9 @@
 namespace HoneyRaesAPI.Models;
 
-public class Customers
+public class Customer
 {
   public int Id { get; set; }
   public string Name { get; set; }
   public string Address { get; set; }
+  public List<ServiceTicket> ServiceTickets { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,95 @@
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Http.Json;
+using HoneyRaesAPI.Models;
+using HoneyRaesAPI.Models.DTOs;
+
+
+// Not needed since data models are using the namespace
+// List<HoneyRaesAPI.Models.Customer> customers = new List<HoneyRaesAPI.Models.Customer> {};
+// List<HoneyRaesAPI.Models.Employee> employees = new List<HoneyRaesAPI.Models.Employee> {};
+// List<HoneyRaesAPI.Models.ServiceTicket> serviceTickets = new List<HoneyRaesAPI.Models.ServiceTicket> {};
+
+List<Customer> customers = new List<Customer>
+{
+    new Customer()
+    {
+        Id = 1,
+        Name = "Janet Hollander",
+        Address = "123 Bender Ln"
+    },
+    new Customer
+    {
+        Id = 2,
+        Name = "Frank Gertz",
+        Address = "456 Tantamount Place"
+    },
+    new Customer
+    {
+        Id = 3,
+        Name = "Verna Ulrich",
+        Address = "789 Grandstand Blvd"
+    }
+};
+List<Employee> employees = new List<Employee>
+{
+    new Employee()
+    {
+        Id = 1,
+        Name = "Rosie Dorfmeister",
+        Specialty = "Screen Repair"
+    },
+    new Employee()
+    {
+        Id = 2,
+        Name = "Brenda Lu",
+        Specialty = "Debugging out"
+    }
+};
+List<ServiceTicket> serviceTickets = new List<ServiceTicket>
+{
+    new ServiceTicket()
+    {
+        Id = 1,
+        CustomerId = 1,
+        EmployeeId = 1,
+        Description = "Cracked Screen",
+        Emergency = false,
+        DateCompleted = new DateTime(2023, 11, 24)
+    },
+    new ServiceTicket()
+    {
+        Id = 2,
+        CustomerId = 2,
+        Description = "Blue screen of death",
+        Emergency = true,
+    },
+    new ServiceTicket()
+    {
+        Id = 3,
+        CustomerId = 3,
+        EmployeeId = 1,
+        Description = "Feelings of despair",
+        Emergency = true,
+    },
+    new ServiceTicket()
+    {
+        Id = 4,
+        CustomerId = 1,
+        EmployeeId = 2,
+        Description = "Button moves slowly",
+        Emergency = false,
+        DateCompleted = new DateTime(2023, 11, 26)
+    },
+    new ServiceTicket()
+    {
+        Id = 5,
+        CustomerId = 2,
+        Description = "Doesn't charge",
+        Emergency = false,
+    }
+ };
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,33 +114,232 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
-app.MapGet("/hello", () =>
+app.MapGet("/servicetickets", () =>
 {
-    return "hello";
+    return serviceTickets.Select(t => new ServiceTicketDTO
+    {
+        Id = t.Id,
+        CustomerId = t.CustomerId,
+        EmployeeId = t.EmployeeId,
+        Description = t.Description,
+        Emergency = t.Emergency,
+        DateCompleted = t.DateCompleted
+    });
+});
+
+app.MapGet("/servicetickets/{id}", (int id) =>
+{
+    ServiceTicket serviceTicket = serviceTickets.FirstOrDefault(st => st.Id == id);
+    if (serviceTicket == null)
+    {
+        return Results.NotFound();
+    }
+    Customer customer = customers.FirstOrDefault(c => c.Id == serviceTicket.CustomerId);
+    Employee employee = employees.FirstOrDefault(e => e.Id == serviceTicket.EmployeeId);
+    return Results.Ok(new ServiceTicketDTO
+    {
+        Id = serviceTicket.Id,
+        CustomerId = serviceTicket.CustomerId,
+        Customer = customer == null ? null : new CustomerDTO
+        {
+            Id = customer.Id,
+            Name = customer.Name,
+            Address = customer.Address
+        },
+        EmployeeId = serviceTicket.EmployeeId,
+        Employee = employee == null ? null : new EmployeeDTO
+        {
+            Id = employee.Id,
+            Name = employee.Name,
+            Specialty = employee.Specialty
+        },
+        Description = serviceTicket.Description,
+        Emergency = serviceTicket.Emergency,
+        DateCompleted = serviceTicket.DateCompleted
+    });
+});
+
+app.MapGet("/employees", () =>
+{
+    return employees.Select(e => new EmployeeDTO
+    {
+        Id = e.Id,
+        Name = e.Name,
+        Specialty = e.Specialty
+    });
+});
+
+app.MapGet("/employees/{id}", (int id) =>
+{
+    Employee employee = employees.FirstOrDefault(e => e.Id == id);
+    if (employee == null)
+    {
+        return Results.NotFound();
+    }
+    List<ServiceTicket> tickets = serviceTickets.Where(st => st.EmployeeId == id).ToList();
+    return Results.Ok(new EmployeeDTO
+    {
+        Id = employee.Id,
+        Name = employee.Name,
+        Specialty = employee.Specialty,
+        ServiceTickets = tickets.Select(t => new ServiceTicketDTO
+        {
+            Id = t.Id,
+            CustomerId = t.CustomerId,
+            EmployeeId = t.EmployeeId,
+            Description = t.Description,
+            Emergency = t.Emergency,
+            DateCompleted = t.DateCompleted
+        }).ToList()
+    });
+});
+
+app.MapGet("/customers", () =>
+{
+    return customers.Select(c => new CustomerDTO
+    {
+        Id = c.Id,
+        Name = c.Name,
+        Address = c.Address
+    });
+});
+
+app.MapGet("/customers/{id}", (int id) =>
+{
+    Customer customer = customers.FirstOrDefault(c => c.Id == id);
+    if (customer == null)
+    {
+        return Results.NotFound();
+    }
+    List<ServiceTicket> tickets = serviceTickets.Where(st => st.CustomerId == id).ToList();
+    return Results.Ok(new CustomerDTO
+    {
+        Id = customer.Id,
+        Name = customer.Name,
+        Address = customer.Address,
+        ServiceTickets = tickets.Select(t => new ServiceTicketDTO
+        {
+            Id = t.Id,
+            CustomerId = t.CustomerId,
+            EmployeeId = t.EmployeeId,
+            Description = t.Description,
+            Emergency = t.Emergency,
+            DateCompleted = t.DateCompleted
+        }).ToList()
+    });
+});
+
+app.MapPost("/servicetickets", (ServiceTicket serviceTicket) =>
+{
+    // creates a new id (SQL will do this for us like JSON Server did!)
+    serviceTicket.Id = serviceTickets.Max(st => st.Id) + 1;
+    // serviceTickets.Add(serviceTicket);
+
+    // Get the customer data to check that the customerid for the service ticket is valid
+    Customer customer = customers.FirstOrDefault(c => c.Id == serviceTicket.CustomerId);
+
+    // if the client did not provide a valid customer id, this is a bad request
+    if (customer == null)
+    {
+        return Results.BadRequest();
+    }
+
+    serviceTickets.Add(serviceTicket);
+
+    // Created returns a 201 status code with a link in the headers to where the new resource can be accessed
+    return Results.Created($"/servicetickets/{serviceTicket.Id}", new ServiceTicketDTO
+    {
+        Id = serviceTicket.Id,
+        CustomerId = serviceTicket.CustomerId,
+        Customer = new CustomerDTO
+        {
+            Id = customer.Id,
+            Name = customer.Name,
+            Address = customer.Address
+        },
+        Description = serviceTicket.Description,
+        Emergency = serviceTicket.Emergency
+    });
+
+});
+
+app.MapDelete("servicetickets/{id}", (int id) =>
+{
+    ServiceTicket ticketToDelete = serviceTickets.FirstOrDefault(st => st.Id == id);
+    if (ticketToDelete == null)
+    {
+        return Results.NotFound();
+    }
+    serviceTickets.Remove(ticketToDelete);
+
+    return Results.NoContent();
+});
+
+app.MapPut("/servicetickets/{id}", (int id, ServiceTicket serviceTicket) =>
+{
+    ServiceTicket ticketToUpdate = serviceTickets.FirstOrDefault(st => st.Id == id);
+
+    if (ticketToUpdate == null)
+    {
+        return Results.NotFound();
+    }
+    if (id != serviceTicket.Id)
+    {
+        return Results.BadRequest();
+    }
+
+    ticketToUpdate.CustomerId = serviceTicket.CustomerId;
+    ticketToUpdate.EmployeeId = serviceTicket.EmployeeId;
+    ticketToUpdate.Description = serviceTicket.Description;
+    ticketToUpdate.Emergency = serviceTicket.Emergency;
+    ticketToUpdate.DateCompleted = serviceTicket.DateCompleted;
+
+    return Results.NoContent();
+});
+
+app.MapPost("/servicetickets/{id}/complete", (int id) =>
+{
+    ServiceTicket ticketToComplete = serviceTickets.FirstOrDefault(st => st.Id == id);
+
+    ticketToComplete.DateCompleted = DateTime.Today;
 });
 
 app.Run();
 
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
+
+
+
+
+// var summaries = new[]
+// {
+//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+// };
+
+// app.MapGet("/weatherforecast", () =>
+// {
+//     var forecast =  Enumerable.Range(1, 5).Select(index =>
+//         new WeatherForecast
+//         (
+//             DateTime.Now.AddDays(index),
+//             Random.Shared.Next(-20, 55),
+//             summaries[Random.Shared.Next(summaries.Length)]
+//         ))
+//         .ToArray();
+//     return forecast;
+// })
+// .WithName("GetWeatherForecast");
+
+// app.MapGet("/hello", () =>
+// {
+//     return "hello";
+// });
+
+
+
+// record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+// {
+//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+// }
